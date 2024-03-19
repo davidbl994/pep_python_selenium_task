8e9c2c7faf67468c213b28f8aaf1ddfc5e5a68d7
davidbl994 <blazevic.david@hotmail.com>
2024-03-19 19:06:53 +0100

    Add print statements for test function test_number_bikes_list

--- tests/test_epower_page/test_number_of_bikes_in_list.py
+++ tests/test_epower_page/test_number_of_bikes_in_list.py
@@ -5,12 +5,15 @@ from ...pages.epower_page import EPowerPage
 @pytest.mark.usefixtures("driver", "epower_url")
 def test_number_bikes_list(driver, epower_url):
     epower_page = EPowerPage(driver)
+    print("Opening ePower page...")
     epower_page.open_epower_page(epower_url)
 
     # Accept cookies
+    print("Accepting cookies...")
     accept_button = epower_page.accept_cookies()
     assert accept_button, "Failed to find the Accept button"
     accept_button.click()
 
     # Check that the bike list is visible and contains nine bikes
+    print("Checking the bike list...")
     assert epower_page.is_bike_list_visible(), "Bike list is not visible or does not contain nine bikes"
