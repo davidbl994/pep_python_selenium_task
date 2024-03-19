cb4b73f0a0867be458753ac9101146a5554544d5
davidbl994 <blazevic.david@hotmail.com>
2024-03-19 18:37:00 +0100

    Add print statements for test func test_change_language_to_german

--- tests/test_home_page/test_change_language_to_german_and_confirm.py
+++ tests/test_home_page/test_change_language_to_german_and_confirm.py
@@ -4,19 +4,24 @@ from ...pages.home_page import HomePage
 @pytest.mark.usefixtures("driver", "home_url")
 def test_change_language_to_german(driver, home_url):
     home_page = HomePage(driver)
+    print("Opening home page...")
     home_page.open_home_page(home_url)
 
     # Accept cookies
+    print("Accepting cookies...")
     accept_button = home_page.accept_cookies()
     assert accept_button, "Failed to find the Accept button"
     accept_button.click()
 
     # Click the German language button
+    print("Clicking the German language button...")
     assert home_page.click_german(), "Failed to click the German language button"
 
     # Confirm the language change
+    print("Confirming the language change...")
     header_text = home_page.get_header_text()
     assert header_text, "Failed to get the header text"
 
+    print("Checking the current URL and header text...")
     assert home_page.driver.current_url == 'https://www.greyp.com/de/', "Failed to navigate to the German home page"
     assert "LIEBE GREYP-GEMEINDE," in header_text, "Failed to change language to German"
