a034a357b4a992734300cab62d0f67102e954d80
davidbl994 <blazevic.david@hotmail.com>
2024-03-19 18:40:00 +0100

    Add print statements for test function test_open_page

--- tests/test_home_page/test_open_page.py
+++ tests/test_home_page/test_open_page.py
@@ -3,16 +3,20 @@ from ...pages.home_page import HomePage
 @pytest.mark.usefixtures("driver", "home_url")
 def test_home_page(driver, home_url):
     home_page = HomePage(driver)
+    print("Opening home page...")
     assert home_page.open_home_page(home_url), "Failed to open the home page"
     assert home_page.driver.current_url == home_url, "Failed to navigate to the home page"
 
     # Call the accept_cookies method without passing any argument
+    print("Accepting cookies...")
     accept_button = home_page.accept_cookies()
     assert accept_button, "Failed to find the Accept button"
 
     # Click the Accept button
+    print("Clicking the Accept button...")
     accept_button.click()
 
     # Confirm the presence of Greyp logo
+    print("Confirming the presence of Greyp logo...")
     assert home_page.is_greyp_logo_displayed(), "Failed to confirm the Greyp logo"
 
